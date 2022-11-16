b67ceccd9e96455118994af167b0bd85864d5631
Murilo Moutinho da Silva <murilo.silva@aluno.feliz.ifrs.edu.br>
2022-11-16 11:50:51 -0300

    up

--- projeto/home.php
+++ projeto/home.php
@@ -10,6 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/commun.css">
+
     <title>IFrame</title>
 </head>
 <body>
@@ -23,75 +24,72 @@ echo $header;
             <div class="container-posts">
 
             
-            <div class="post">
-                
-                <div class="post-info">
-                    <img  src="assets/istockphoto-1016744034-170667a.jpg" alt="">
-                    <div>
-                        <p class="">Nome Usuario</p>
-                        <p>turma A1</p>
+                <div class="post">
+                    
+                    <div class="post-info">
+                        <img  src="assets/istockphoto-1016744034-170667a.jpg" alt="">
+                        <div>
+                            <p class="">Nome Usuario</p>
+                            <p>turma A1</p>
+                        </div>
                     </div>
-                </div>
-            
-                <div class="post-img">
-                    <img class="img-format" src="assets/img.jpg" alt="">
-                </div>
-                <img src="<?php echo $imgLike ?>" class="like" alt="">
-            </div>
-
-            <div class="post">
                 
-                <div class="post-info">
-                    <img  src="assets/istockphoto-1016744034-170667a.jpg" alt="">
-                    <div>
-                        <p class="">Nome Usuario</p>
-                        <p>turma A1</p>
+                    <div class="post-img">
+                        <img class="img-format" src="assets/img.jpg" alt="">
                     </div>
+                    <img src="<?php echo $imgLike ?>" class="like" alt="">
                 </div>
-            
-                <div class="post-img">
-                    <img class="img-format" src="assets/photo-1606946887361-78feb162a525.jfif" alt="">
-                </div>
-                <img src="<?php echo $imgLike ?>" class="like" alt="">
 
-            </div>
-
-            <div class="post">
+                <div class="post">
+                    
+                    <div class="post-info">
+                        <img  src="assets/istockphoto-1016744034-170667a.jpg" alt="">
+                        <div>
+                            <p class="">Nome Usuario</p>
+                            <p>turma A1</p>
+                        </div>
+                    </div>
                 
-                <div class="post-info">
-                    <img src="assets/istockphoto-1016744034-170667a.jpg" alt="">
-                    <div>
-                        <p class="">Nome Usuario</p>
-                        <p>turma A1</p>
+                    <div class="post-img">
+                        <img class="img-format" src="assets/photo-1606946887361-78feb162a525.jfif" alt="">
                     </div>
-                </div>
-            
-                <div class="post-img">
-                    <img class="img-format" src="assets/espacoo.jpg" alt="">
-                </div>
-                <img src="<?php echo $imgLike ?>" class="like" alt="">
+                    <img src="<?php echo $imgLike ?>" class="like" alt="">
 
-            </div>
+                </div>
 
-            <div class="post">
+                <div class="post">
+                    
+                    <div class="post-info">
+                        <img src="assets/istockphoto-1016744034-170667a.jpg" alt="">
+                        <div>
+                            <p class="">Nome Usuario</p>
+                            <p>turma A1</p>
+                        </div>
+                    </div>
                 
-                <div class="post-info">
-                    <img src="assets/istockphoto-1016744034-170667a.jpg" alt="">
-                    <div>
-                        <p class="">Nome Usuario</p>
-                        <p>turma A1</p>
+                    <div class="post-img">
+                        <img class="img-format" src="assets/espacoo.jpg" alt="">
                     </div>
+                    <img src="<?php echo $imgLike ?>" class="like" alt="">
+
                 </div>
-            
-                <div class="post-img">
-                    <img class="img-format" src="assets/gif.gif" alt="">
+
+                <div class="post">
+                    
+                    <div class="post-info">
+                        <img src="assets/istockphoto-1016744034-170667a.jpg" alt="">
+                        <div>
+                            <p class="">Nome Usuario</p>
+                            <p>turma A1</p>
+                        </div>
+                    </div>
+                
+                    <div class="post-img">
+                        <img class="img-format" src="assets/gif.gif" alt="">
+                    </div>
+                    <img src="<?php echo $imgLike ?>" class="like" alt="">
+
                 </div>
-                <img src="<?php echo $imgLike ?>" class="like" alt="">
-
-            </div>
-
-
-        
 
         </div>
 
--- projeto/src/User.php
+++ projeto/src/User.php
@@ -3,45 +3,49 @@
 class User implements ActiveRecord{
 
     private int $idUsuario;
+    private int $foto;
+    private int $bio;
 
-    public function __construct(private string $email,private string $senha){
+
+    public function __construct(private string $email,private string $senha,private string $nome, private string $turma){
     }
 
-    public function setIdUsuario(int $idUsuario):void{
-        $this->idUsuario = $idUsuario;
-    }
 
+    //ID ------------------------------------------------
     public function getIdUsuario():int{
         return $this->idUsuario;
     }
 
+    //SENHA ------------------------------------------------
     public function setSenha(string $senha):void{
         $this->senha = $senha;
-    }
-
-    public function setEmail(string $email):void{
-        $this->email = $email;
     }
 
     public function getSenha():string{
         return $this->senha;
     }
 
+    //EMAIL ------------------------------------------------
     public function getEmail():string{
         return $this->email;
     }
 
+
+
+
+    //SALVAR ------------------------------------------------
     public function save():bool{
         $conexao = new MySQL();
         $this->senha = password_hash($this->senha,PASSWORD_BCRYPT); 
         if(isset($this->idUsuario)){
-            $sql = "UPDATE usuarios SET email = '{$this->email}' ,senha = '{$this->senha}' WHERE idUsuario = {$this->idUsuario}";
+            $sql = "UPDATE usuario SET email = '{$this->email}' ,senha = '{$this->senha}' ,nome = '{$this->nome} ,turma = '{$this->turma}' WHERE id = {$this->idUsuario}";
         }else{
-            $sql = "INSERT INTO usuarios (email,senha) VALUES ('{$this->email}','{$this->senha}')";
+            $sql = "INSERT INTO usuario (email,senha,nome,turma) VALUES ('{$this->email}','{$this->senha}','{$this->nome}','{$this->turma}')";
         }
         return $conexao->executa($sql);
     }
 
+    //FIND ------------------------------------------------
     public static function find($idUsuario):Usuario{
         $conexao = new MySQL();
         $sql = "SELECT * FROM usuarios WHERE idUsuario = {$idUsuario}";
@@ -50,13 +54,6 @@ class User implements ActiveRecord{
         $u->setIdUsuario($resultado[0]['idUsuario']);
         return $u;
     }
-
-    public function delete():bool{
-        $conexao = new MySQL();
-        $sql = "DELETE FROM usuarios WHERE idUsuario = {$this->idUsuario}";
-        return $conexao->executa($sql);
-    }
-
     public static function findall():array{
         $conexao = new MySQL();
         $sql = "SELECT * FROM usuarios";
@@ -70,13 +67,22 @@ class User implements ActiveRecord{
         return $usuarios;
     }
 
+
+    //DELETE ------------------------------------------------
+    public function delete():bool{
+        $conexao = new MySQL();
+        $sql = "DELETE FROM usuario WHERE id = {$this->idUsuario}";
+        return $conexao->executa($sql);
+    }
+
+    //AUTENTICAR ------------------------------------------------
     public function authenticate():bool{
         $conexao = new MySQL();
-        $sql = "SELECT idUsuario,senha FROM usuarios WHERE email = '{$this->email}'";
+        $sql = "SELECT id,senha FROM usuario WHERE email = '{$this->email}'";
         $resultados = $conexao->consulta($sql);
         if(password_verify($this->senha,$resultados[0]['senha'])){
             session_start();
-            $_SESSION['idUsuario'] = $resultados[0]['idUsuario'];
+            $_SESSION['id'] = $resultados[0]['id'];
             $_SESSION['email'] = $resultados[0]['email'];
             return true;
         }else{
