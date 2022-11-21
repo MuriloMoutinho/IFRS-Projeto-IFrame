dc95f53b82d2a4f809a3d15d5228d0b3ee3403a6
Nícolas Augusto Köhler Arend <107890287+NicolasIFRS@users.noreply.github.com>
2022-11-21 09:55:20 -0300

    login

--- projeto/login.php
+++ projeto/login.php
@@ -28,20 +28,27 @@ if(isset($_POST['submit'])){
 </head>
 <body>
 
-    <div class="center"> 
-        <div class="column login-box">
-            
-            <h1>Login</h1>
-            <form  action='login.php' method='post' class="column">
-                <label>Email <input type="email" name='email' required></label>
-                <label>Password <input type="password" name='password' required></label>
-                <input type="submit" value="login" name='submit'>
-                <a href="index.php">Back</a>
-            </form>
+    <div class="container-login"> 
+        <div class="column login-all-box">
+            <div class="login-box">
 
+            
+                <div class="title-login">
+                    <h1>Login</h1>
+                </div>
+            
+                <div class="input-box">
+                    <form  action='login.php' method='post' class="column">
+                        <label>Email <input type="email" name='email' required></label>
+                        <label>Password <input type="password" name='password' required></label>
+                        <input type="submit" value="login" name='submit'>
+                        <a href="index.php">Back</a>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 
-    <a href="home.php">Link temporario que redireciona para a HOME</a>
+    
 </body>
 </html>
