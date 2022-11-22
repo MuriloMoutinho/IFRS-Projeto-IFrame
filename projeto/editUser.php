9bea19f14a34e7e4f7ad983991a0e2dd68c71a5c
MuriloMoutinho <murilo.silva@aluno.feliz.ifrs.edu.br>
2022-11-22 15:01:28 -0300

    começando sistema likes

--- projeto/editUser.php
+++ projeto/editUser.php
@@ -57,6 +57,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/commun.css">
     <link rel="stylesheet" href="css/register.css">
+    <link rel="shortcut icon" href="assets/favicon-32x32.png" type="image/x-icon">
     <title>Profile</title>
 </head>
 <body>
--- projeto/newPhoto.php
+++ projeto/newPhoto.php
@@ -24,6 +24,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/commun.css">
     <link rel="stylesheet" href="css/newPhoto.css">
+    <link rel="shortcut icon" href="assets/favicon-32x32.png" type="image/x-icon">
     <title>IFrame</title>
 </head>
 <body>
