3ba69472af26271eb2af5c8d445adeec62a9b15a
Nícolas Augusto Köhler Arend <107890287+NicolasIFRS@users.noreply.github.com>
2022-11-28 21:45:34 -0300

    home

--- projeto/components/header.php
+++ projeto/components/header.php
@@ -2,8 +2,11 @@
 
 $header = "
     <header class='header'>
-        <div class='container flex-row-short'>        
+        <div class='container-header flex-row-short'>      
                 <img src='$imglogo' alt='Iframe logo'>
-                <h1>IFrame</h1>      
+                <h1>IFrame</h1>
+                <div class='logout-header'>
+                    <a href='config/logout.php'><img src='assets/icos/out_ico2.png' alt=''>Sign out</a>
+                </div> 
         </div>
     </header>";
