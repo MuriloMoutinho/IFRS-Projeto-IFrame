262cebe44853151eafec9e403454b6e770957749
Fábio Júnior Nielsson Bastian <fabiobastian44@gmail.com>
2022-11-29 20:57:31 -0300

    recorte css
    recorte de vários elementos para tentar otimizar ao máximo

--- projeto/editUser.php
+++ projeto/editUser.php
@@ -51,8 +51,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/commun.css">
-    <link rel="stylesheet" href="css/register.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/communFabio.css">
+    <link rel="stylesheet" href="css/registerFabio.css">
     <link rel="shortcut icon" href="assets/favicon-32x32.png" type="image/x-icon">
     <title>Profile</title>
 </head>
