736893b338f325c19d4661624ba5911fc57b6c65
Fábio Júnior Nielsson Bastian <fabiobastian44@gmail.com>
2022-11-30 10:01:00 -0300

    up correcao de erros

--- projeto/components/header.php
+++ projeto/components/header.php
@@ -1,12 +1,17 @@
 <?php
 
 $header = "
-    <header class='header'>
-        <div class='container-header flex-row-short'>      
-                <img src='$imglogo' alt='Iframe logo'>
-                <h1>IFrame</h1>
-                <div class='logout-header'>
-                    <a href='config/logout.php'><img src='assets/icos/out_ico2.png' alt=''>Sign out</a>
-                </div> 
+<header class='header'>
+    <div class='container-header flex-row-short'>
+        <div class='header-logo'>
+            <img src='$imglogo' alt='Iframe logo' class='header-icon'>
+            <h1>IFrame</h1>
         </div>
-    </header>";
+        <div class='header-logout'>
+            <a href='config/logout.php'>
+                <img src='assets/icos/out_ico2.png' alt='Sign out' class='header-icon'>
+                <span>Sign out</span>
+            </a>
+        </div> 
+    </div>
+</header>";
--- projeto/editUser.php
+++ projeto/editUser.php
@@ -52,6 +52,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/communFabio.css">
     <link rel="stylesheet" href="css/registerFabio.css">
     <link rel="shortcut icon" href="assets/favicon-32x32.png" type="image/x-icon">
@@ -61,64 +62,51 @@
     
     <?php
     echo $menu;
-
     echo $header;
     ?>
 
     <main>
-
-    <div class="center container"> 
-        <div class="login-box">
-            <h1>Edit</h1>
-            <form action="editUser.php" method="post" class="column" enctype="multipart/form-data">
-                <?php
-
-                echo "<label for='userName'>UserName: </label>";
-                echo "<input type='text' name='name' id='userName' class='input' maxlength='25' minlength='2' value='{$usuarioConsulta->getNome()}' required>";
-                echo "<label for='email'>Email: </label>";
-                echo "<input type='email' name='email' id='email' value='{$usuarioConsulta->getEmail()}' required>";
+        <div class="center container"> 
+            <div class="login-box">
+                <h1>Edit</h1>
+                <form action="editUser.php" method="post" class="column" enctype="multipart/form-data">
+                    <?php
+                    echo "<label for='userName'>UserName: </label>";
+                    echo "<input type='text' name='name' id='userName' class='input' maxlength='25' minlength='2' value='{$usuarioConsulta->getNome()}' required>";
+                    echo "<label for='email'>Email: </label>";
+                    echo "<input type='email' name='email' id='email' value='{$usuarioConsulta->getEmail()}' required>";
                 
-                echo "<label for='turma'>Escolha a sua turma: </label>";
-                echo "<select id='turma' name='turma'>";
+                    echo "<label for='turma'>Escolha a sua turma: </label>";
+                    echo "<select id='turma' name='turma'>";
                 
-                $conexao = new MySQL();
-                $sql = "SELECT * FROM turma order by id asc";
-                $turmas = $conexao->consulta($sql);
+                    $conexao = new MySQL();
+                    $sql = "SELECT * FROM turma order by id asc";
+                    $turmas = $conexao->consulta($sql);
 
-                foreach($turmas as $turma){ 
-
-                    echo "<option ".($turma['id'] == $usuarioConsulta->getTurma() ? "selected":"")." value='{$turma['id']}'>{$turma['curso']}</option>";
+                    foreach($turmas as $turma){ 
+                        echo "<option ".($turma['id'] == $usuarioConsulta->getTurma() ? "selected":"")." value='{$turma['id']}'>{$turma['curso']}</option>";
                     }
                         
-                echo "</select>";
-                echo "<label for='biografia'>Biografia: </label>";
-                echo "<textarea name='bio' id='biografia' cols='20' rows='6' >{$usuarioConsulta->getBio()}</textarea>";
-                
-                ?>
-                <label for="foto" >Foto: </label>
-                <input type='file' accept='image/*' name='foto' id="foto"> 
-                <input type='submit' value='remove photo' name='remove'>
-                
-                <label for="currentPass">Write your current password:</label>
-                <input type='password' name='currentPass' id="currentPass">
-                <label for="newPassword" >write your new password: </label>
-                <input type='password' minlength='3' name='newPassword' id="newPassword">
-
-                <input type='submit' value='Edit' name='submit'>
-                
-            </form>
-            <a href="config/deleteUser.php">Deletar conta</a>
-            <a href="index.php">Back</a>
+                    echo "</select>";
+                    echo "<label for='biografia'>Biografia: </label>";
+                    echo "<textarea name='bio' id='biografia' cols='20' rows='6' >{$usuarioConsulta->getBio()}</textarea>";
+                    ?>
+                    <label for="foto" >Foto: </label>
+                    <input type='file' accept='image/*' name='foto' id="foto"> 
+                    <input type='submit' value='remove photo' name='remove'>
+                    <label for="currentPass">Write your current password:</label>
+                    <input type='password' name='currentPass' id="currentPass">
+                    <label for="newPassword" >write your new password: </label>
+                    <input type='password' minlength='3' name='newPassword' id="newPassword">
+                    <input type='submit' value='Edit' name='submit'>
+                </form>
+                <a href="config/deleteUser.php">Deletar conta</a>
+                <a href="index.php">Back</a>
+            </div>
         </div>
-    </div>
-
     </main>
-
-
-    
     <?php
     echo $footer;
-    ?>   
-
-</body>
+    ?>
+    </body>
 </html>
