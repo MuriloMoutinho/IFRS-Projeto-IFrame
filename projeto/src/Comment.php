a52ff5bbb2de99e25b7f42ec7d0bc480abc29eff
Fábio Júnior Nielsson Bastian <fabiobastian44@gmail.com>
2022-12-16 11:39:11 -0300

    conserto barras

--- projeto/src/Comment.php
+++ projeto/src/Comment.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__."\Post.php";
-require_once __DIR__."\ActiveRecord.php";
+require_once __DIR__."/Post.php";
+require_once __DIR__."/ActiveRecord.php";
 
 class Comment implements ActiveRecord{
     
--- projeto/src/Post.php
+++ projeto/src/Post.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__."\User.php";
-require_once __DIR__."\ActiveRecord.php";
+require_once __DIR__."/User.php";
+require_once __DIR__."/ActiveRecord.php";
 
 class Post implements ActiveRecord{
 
