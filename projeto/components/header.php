58c0f2185320b5f7c3197c2eb59bdb91fc2fe2cd
Murilo Moutinho da Silva <murilo.silva@aluno.feliz.ifrs.edu.br>
2022-12-19 11:55:45 -0300

    header

--- projeto/components/header.php
+++ projeto/components/header.php
@@ -6,8 +6,8 @@ $header = "
                  <div class='title-header flex-row-short'>
                     <img src='$imglogo' alt='Iframe logo'>
                     <h1>IFrame</h1>
-                    </a>
-                </div>
+                    </div>
+                </a>
 
                 <div class='logout-header flex-row-short'>
                     <a href='config/logout.php'><img src='assets/icos/out_ico2.png' alt=''>Sign out</a>
